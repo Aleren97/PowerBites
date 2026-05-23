6f2cda5335a6423c14184a3f3fa846aeb1d55ace
Aleren97 <renzoalejandro26@gmail.com>
2026-05-23 17:49:54 +0200

    Adding entitie Persona.java.

--- JAVA/PowerBites/src/main/java/com/powerbites/entities/Persona.java
+++ JAVA/PowerBites/src/main/java/com/powerbites/entities/Persona.java
@@ -2,23 +2,23 @@ package com.powerbites.entities;
 
 public abstract class Persona {
     protected int id;
-    protected String nombre;
+    protected String name;
     protected String email;
 
-    public Persona(int id, String nombre, String email) {
+    public Persona(int id, String name, String email) {
         this.id = id;
-        this.nombre = nombre;
+        this.name = name;
         this.email = email;
     }
 
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
 
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
 
-    public abstract void mostrarDetalles();
+    public abstract void showDetails();
 }
